6ef4efdde6af382405573fba17c531fa31b3f97a
Paul <azrael7@gmail.com>
2021-02-10 22:47:25 -0600

    Added orders fetch from json.

--- src/Orders.tsx
+++ src/Orders.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import { Order } from './Interfaces/EmeraldTypes';
 // import { Container, Row, Col, Button, Jumbotron } from 'react-bootstrap';
 // import { Link } from 'react-router-dom';
 
 function Orders() {
-  return <div>Orders here</div>;
+  const [orders, ordersSet] = React.useState<Order[]>([]);
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/Abooth8503/emeraldbakery/master/orders.json')
+      .then((resp) => resp.json())
+      .then((tds: Order[]) => {
+        console.log('orders', tds);
+        ordersSet(tds);
+      });
+  }, []);
+
+  return <div>Orders here {orders.length}</div>;
 }
 
 export default Orders;
