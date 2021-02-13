b2a1ca8d1191a5955be3903632c1f6853960b393
Paul <azrael7@gmail.com>
2021-02-13 13:02:22 -0600

    Added google autocomplete address.

--- src/Forms/CreateOrder.tsx
+++ src/Forms/CreateOrder.tsx
@@ -1,7 +1,66 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import * as React from 'react';
 import { Container, Jumbotron, Form, Button } from 'react-bootstrap';
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
+import useOnclickOutside from 'react-cool-onclickoutside';
 
 function CreateOrder() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e: any) => {
+    console.log(e.target.value);
+    // Update the keyword of the input element
+    setValue(e.target.value, true);
+  };
+
+  const handleSelect = ({ description }: { description: any }) => () => {
+    // When user selects a place, we can replace the keyword without request data from API
+    // by setting the second parameter to "false"
+    setValue(description, false);
+    clearSuggestions();
+
+    // Get latitude and longitude via utility functions
+    getGeocode({ address: description })
+      .then((results) => getLatLng(results[0]))
+      .then(({ lat, lng }) => {
+        console.log('📍 Coordinates: ', { lat, lng });
+      })
+      .catch((error) => {
+        console.log('😱 Error: ', error);
+      });
+  };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
   return (
     <Container>
       <Jumbotron style={{ backgroundColor: 'lightgreen', marginTop: '3px' }}>
@@ -13,6 +72,18 @@ function CreateOrder() {
           <Form.Label>Name</Form.Label>
           <Form.Control type='text' placeholder='Enter name of order' />
         </Form.Group>
+
+        <div ref={ref}>
+          <input
+            value={value}
+            onChange={handleInput}
+            disabled={!ready}
+            placeholder='Where are you going?'
+            style={{ width: '100%' }}
+          />
+          {/* We can use the "status" to decide whether we should display the dropdown or not */}
+          {status === 'OK' && <ul>{renderSuggestions()}</ul>}
+        </div>
 
         <Form.Group controlId='formBasicPassword'>
           <Form.Label>Address</Form.Label>
