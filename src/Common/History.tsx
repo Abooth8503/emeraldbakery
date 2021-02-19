5351508f2e52b255dd7929eca606e648339a9623
Paul <azrael7@gmail.com>
2021-02-19 14:02:33 -0600

    Updated history.

--- src/Common/History.tsx
+++ src/Common/History.tsx
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
   baseNameProd = '';
 } else {
-  baseNameProd = '/emeraldcity';
+  baseNameProd = '/emeraldbakery';
 }
 
 const customHistory = createBrowserHistory({
