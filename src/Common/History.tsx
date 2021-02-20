a55fb4502f12d334cebc5bd0e1365e3ae14336a7
Paul <azrael7@gmail.com>
2021-02-19 23:11:49 -0600

    Changed baseNameProd.

--- src/Common/History.tsx
+++ src/Common/History.tsx
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
   baseNameProd = '';
 } else {
-  baseNameProd = '/';
+  baseNameProd = '/emeraldbakery';
 }
 
 const customHistory = createBrowserHistory({
